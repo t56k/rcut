605b5c91118d2dcb3fb8bedca6aa00beaa8f7004
t56k <txnash@gmail.com>
2021-11-12 21:40:51 +1300

    clean errors

--- src/error.rs
+++ src/error.rs
@@ -13,17 +13,8 @@ pub enum Error {
     #[snafu(display("Failed to read dimensions from ffmpeg stdout."))]
     FFMPEGLineReadError,
 
-    #[snafu(display("Could not parse video dimensions. {}", source))]
-    ParseDimensionsError { source: std::num::ParseIntError },
-
     #[snafu(display("Could not parse video duration. {}", source))]
     ParseDurationError { source: std::num::ParseFloatError },
-
-    #[snafu(display("ffmpeg could not provide a handle to stdout."))]
-    StdoutHandleError,
-
-    #[snafu(display("ffmepg encountered an error. {}", source))]
-    FfmpegError { source: io::Error },
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
